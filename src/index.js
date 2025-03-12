075241b2b3fbd96acdef8e971d8d18926acb8b8f
Thandokazi-coder <payithando@gmail.com>
2025-03-12 18:51:45 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function generatorPoem(event) {
     event.preventDefault()
+    
     let poemElement = document.querySelector("poem");
     new Typewriter('#poem', {
         strings: "I Love You",
